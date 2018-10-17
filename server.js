d7145ccd3d7d0a1e7e859827913cc8553979191d
Luc.A <luc.bayo@gmail.com>
2018-10-17 10:07:36 +0200

    Installed required dependencies,and setted Dev envirnoment

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ working: "sucess" });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`the app is listening to ${port}`));
